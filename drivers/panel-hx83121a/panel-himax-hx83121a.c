4269e32bff70f9f3e20acf41e207d37014478cc8
psacal <2658842706@qq.com>
2026-06-07 15:15:11 +0000

    Revert "fix: add drm_connector_set_panel_orientation in get_modes"
    This reverts commit 7fd3fd37ba0a028df4b976c3a783048d6330b3a4.

--- drivers/panel-hx83121a/panel-himax-hx83121a.c
+++ drivers/panel-hx83121a/panel-himax-hx83121a.c
@@ -17,7 +17,6 @@
 
 #include <drm/display/drm_dsc.h>
 #include <drm/display/drm_dsc_helper.h>
-#include <drm/drm_connector.h>
 #include <drm/drm_mipi_dsi.h>
 #include <drm/drm_modes.h>
 #include <drm/drm_panel.h>
@@ -183,9 +182,6 @@ static int himax_get_modes(struct drm_panel *panel,
 	connector->display_info.width_mm = desc->width_mm;
 	connector->display_info.height_mm = desc->height_mm;
 	connector->display_info.bpc = desc->bpc;
-
-	drm_connector_set_panel_orientation(connector,
-					    DRM_MODE_PANEL_ORIENTATION_NORMAL);
 
 	return num_modes;
 }
